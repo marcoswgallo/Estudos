97f7b07c0136e58900ce7bc4854fe23006bf1de0
marcoswgallo <70547583+marcoswgallo@users.noreply.github.com>
2020-09-18 21:34:57 -0300

    Add files via upload

---
+++ Pass_ e _Ellipsis_como_placeholders.py
@@ -0,0 +1,15 @@
+"""
+Data : 18/09/2020
+Curso : Python 3 do Básico ao Avançado
+Aluno : Marcos Gallo
+Assunto da Aula : Pass e Ellipsis como placeholders
+"""
+
+# pass
+# ...
+
+valor = True
+if valor:
+    pass
+else:
+    print('Tchau')
