c5af99c03f7c40b349c080510293abcec8929960
marcoswgallo <70547583+marcoswgallo@users.noreply.github.com>
2020-09-18 21:28:47 -0300

    Add files via upload

---
+++ documentacao_funcoes_built-in.py
@@ -0,0 +1,19 @@
+"""
+Data : 18/09/2020
+Curso : Python 3 do Básico ao Avançado
+Aluno : Marcos Gallo
+Assunto da Aula : Documentação e funções built-in úteis
+"""
+
+num1 = input('Digite um número: ')
+num2 = input('Digite um número: ')
+
+if num1.isdigit() and num2.isdigit():
+    num1 = int(num1)
+    num2 = int(num2)
+
+    print(num1 + num2)
+else:
+    print('Não pude converter os números para realizar contas.')
+
+
